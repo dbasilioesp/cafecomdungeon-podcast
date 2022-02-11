467f3f941f6c97c6ee3bd28b633b43cf36ab924d
David Basilio E <dbasilioesp@gmail.com>
2022-02-11 13:50:10 -0300

    feat: search engine callback working

--- src/views/App.js
+++ src/views/App.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import ENV from '../env';
 import Card from '../components/card/Card'
@@ -8,77 +8,59 @@ import mixpanel from '../plugins/mixpanel';
 
 mixpanel.track('Page Loaded');
 
-const LIMIT = 20;
+let timer;
 
 function App() {
   const [episodes, setEpisodes ] =  useState([])
-  const [currentPage, setPage ] =  useState(1)
-  const [total, setTotal] = useState(9999)
   const [loading, setLoading] = useState(false)
 
-  const getEpisodes = useCallback(async () => {
+  useEffect(async () => {
     setLoading(true);
-    const offset = (currentPage - 1) * (LIMIT + 1);
-    const config = {params: { limit: LIMIT, offset }}
-    const response = await axios.get(`${ENV.api}/episodes`, config);
-    setTotal(response.data.total)
-    setEpisodes([...episodes, ...response.data.items]);
+    const response = await axios.get(`${ENV.api}/episodes`);
+    setEpisodes([...episodes, ...response.data.data]);
     setLoading(false);
-  }, [setLoading, setTotal, setEpisodes, episodes, currentPage])
+  }, [])
 
-  const intersectPagination = useCallback(() => {
-    const callbackObserver = (entries, observer) => {
-      for (const entry of entries) {
-        if(entry.isIntersecting){
-          observer.unobserve(entry.target);
-          setPage(currentPage + 1);
-          mixpanel.track('Paginating', {page: currentPage + 1});
-        }
-      }
+  const handleChange = (event) => {
+    setLoading(true);
+    if (timer) {
+      clearTimeout(timer)
     }
-    const configObserver = {rootMargin: '0px 0px -90px 0px'};
-    const observer = new IntersectionObserver(callbackObserver, configObserver);
-    observer.observe(document.querySelector('footer'))
-  }, [currentPage, setPage])
-  
-  useEffect(() => {
-    if (episodes.length === 0) {
-      window.scrollTo(0, 0)
-    }
-    getEpisodes()
-  }, [currentPage])
 
-  useEffect(() => {
-    if (episodes.length > 0 && episodes.length < total){
-      setTimeout(() => {
-        intersectPagination()
-      }, 1000);
-    }
-  }, [episodes])
+    timer = setTimeout(async () => {
+      const config = { params: { q: event.target.value }}
+      const response = await axios.get(`${ENV.api}/episodes`, config);
+      setEpisodes(response.data.data);
+      setLoading(false);
+    }, 600);
+  }
 
   return (
     <div className="app">
       <header className="app__header">
         <h1>Café com Dungeon</h1>
-        </header>
-        <main className="card-grid">
-          {
-            episodes.map((item, index) => {
-              return (
-                <Card 
-                  id={item.id}
-                  title={item.name}
-                  description={item.html_description}
-                  link={item.uri}
-                  key={index}
-                />
-              )
-            })
-          }
-        </main>
-        <footer className="app__footer">
-          {loading && 'Carregando mais episódios...'}
-        </footer>
+        <form>
+          <input type="text" class="search"  placeholder="Pesquise.." aria-label='Pesquisa' onChange={handleChange} />
+        </form>
+      </header>
+      <main className="card-grid">
+        {
+          episodes.map((item, index) => {
+            return (
+              <Card 
+                id={item.id}
+                title={item.name}
+                description={item.htmlDescription}
+                link={item.hosts[0].uri}
+                key={index}
+              />
+            )
+          })
+        }
+      </main>
+      <footer className="app__footer">
+        {loading && 'Carregando mais episódios...'}
+      </footer>
     </div>
   );
 }
