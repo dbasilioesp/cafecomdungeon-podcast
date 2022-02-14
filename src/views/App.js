191554fbe9c299cd0452b4d6110741d2b8c0e130
David Basilio E <dbasilioesp@gmail.com>
2022-02-14 20:57:11 -0300

    feat: search and next load

--- src/views/App.js
+++ src/views/App.js
@@ -9,36 +9,54 @@ import mixpanel from '../plugins/mixpanel';
 mixpanel.track('Page Loaded');
 
 let timer;
+const LIMIT = 20;
 
 function App() {
+
   const [episodes, setEpisodes ] =  useState([])
   const [loading, setLoading] = useState(false)
+  const [page, setPage ] =  useState(1)
+  const [total, setTotal] = useState(9999)
+  const [search, setSearch] = useState(null)
+
+  async function nextPage(pageParam) {
+    setPage(pageParam)
+    await getEpisodes(pageParam, search);
+  }
+
+  async function getEpisodes(page, search) {
+    const config = { params: { page, q: search }}
+    
+    const { data } = await axios.get(`${ENV.api}/episodes`, config);
+    setTotal(data.total);
+    setEpisodes([...data.data]);
+  }
+
+  function searchEpisodes(searchParam) {
+    setLoading(true);
+    if (timer) { clearTimeout(timer) }
+
+    timer = setTimeout(async () => {
+      setSearch(searchParam);
+      await getEpisodes(page, searchParam);
+      setLoading(false);
+    }, 600);
+  }
 
   useEffect(async () => {
     setLoading(true);
-    const response = await axios.get(`${ENV.api}/episodes`);
-    setEpisodes([...episodes, ...response.data.data]);
+    await getEpisodes(page);
     setLoading(false);
   }, [])
 
   const handleChange = (event) => {
-    setLoading(true);
-    if (timer) {
-      clearTimeout(timer)
-    }
-
-    timer = setTimeout(async () => {
-      const config = { params: { q: event.target.value }}
-      const response = await axios.get(`${ENV.api}/episodes`, config);
-      setEpisodes(response.data.data);
-      setLoading(false);
-    }, 600);
+    searchEpisodes(event.target.value)
   }
 
   return (
     <div className="app">
       <header className="app__header">
-        <h1>Café com Dungeon</h1>
+        <h1>Café com Dungeon - {total}</h1>
         <form>
           <input type="text" class="search"  placeholder="Pesquise.." aria-label='Pesquisa' onChange={handleChange} />
         </form>
