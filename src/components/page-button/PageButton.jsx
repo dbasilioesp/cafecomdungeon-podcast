08e24d82fc6238c1a13ea44f05fd7e211a9c67cf
David Basilio E <dbasilioesp@gmail.com>
2022-02-18 20:06:52 -0300

    feat: dark background of page

--- src/components/page-button/PageButton.jsx
+++ src/components/page-button/PageButton.jsx
@@ -1,6 +1,6 @@
 import './page-button.css'
 
-export default function({children, disabled, onClick}) {
+export default function PageButton({children, disabled, onClick}) {
   return (
     <button className="page-button" type="button" disabled={disabled} onClick={onClick}>
       {children}
--- src/components/search-input/SearchInput.jsx
+++ src/components/search-input/SearchInput.jsx
@@ -1,6 +1,6 @@
 import './search-input.css'
 
-export default function ({total, onChange}) {
+export default function SearchInput({total, onChange}) {
   return (
     <div className="search">
       <input type="text"   placeholder="Pesquise.." aria-label='Pesquisa' onChange={onChange} />
